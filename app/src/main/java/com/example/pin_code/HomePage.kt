f804e1a2f9432ea27e197bdf32ef0b51659bba86
vladk <vladkobets0@gmail.com>
2024-06-14 22:19:36 +0500

    first commit

---
+++ app/src/main/java/com/example/pin_code/HomePage.kt
@@ -0,0 +1,34 @@
+package com.example.pin_code
+
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+lateinit var cls: Button
+
+class HomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        cls = findViewById(R.id.close_but)
+
+        cls.setOnClickListener{view: View ->
+            this.finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pin_code/StartPage.kt
@@ -0,0 +1,61 @@
+package com.example.pin_code
+
+import android.R.attr.value
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+
+lateinit var pinEditText: EditText
+lateinit var result: TextView
+lateinit var pref: SharedPreferences
+
+
+
+class StartPage : AppCompatActivity() {
+    var pincode = 0
+    var count = 0
+    var pref: SharedPreferences? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_start_page)
+        pinEditText = findViewById(R.id.pin_code)
+        result = findViewById(R.id.result)
+
+        pref = getSharedPreferences("TABLE2", Context.MODE_PRIVATE)
+        pincode = pref?.getInt("code", 0)!!
+        result.text = pincode.toString()
+
+        if(pincode != 0){
+            val myIntent: Intent = Intent(
+                this@StartPage,
+                HomePage::class.java
+            )
+            this@StartPage.startActivity(myIntent)
+        }
+    }
+
+    fun savePin(res: Int){
+        val editor = pref?.edit()
+        editor?.putInt("code", res)
+        editor?.apply()
+    }
+
+    fun onClickSave(view: View) {
+        pincode = Integer.parseInt(pinEditText.getText().toString())
+        savePin(pincode)
+        val myIntent: Intent = Intent(
+            this@StartPage,
+            HomePage::class.java
+        )
+        this@StartPage.startActivity(myIntent)
+    }
+
+}
